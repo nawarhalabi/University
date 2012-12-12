011ee37438ea05457109317ba435693f0e5ec964
nawarhalabi <nawar.halabi@gmail.com>
2012-12-12 17:40:21 +0000

    3.0 Minor changes

--- ReSTfulWebServiceProject/routes/album.js
+++ ReSTfulWebServiceProject/routes/album.js
@@ -1,29 +1,41 @@
+function createSchema()
+{
+	var albums = new mongoose.Schema({
+	name: {type: String, requiered: true},
+	author: { type: String, required: true, ref: 'author'},
+	metadata: {
+		description: String,
+		date_added: { type: Date, default: Date.now }},
+	comments: [{
+		date: { type: Date, default: Date.now },
+		text: String,
+		author: { type: String, required: true, ref: 'author' }
+	}]
+	});
+	albums.index({name:1,author:1},{unique:true});
+	
+	return albums;
+}
 
+exports.add = function(req, res)
+{
+	var mongoose = require('mongoose');
+	var conn = mongoose.createConnection('mongodb://localhost/Gallerydb');
+	var albums = createSchema();
+	var albumModel = conn.model('album', albums);
+	
+	function adderr(err){console.log('Add Error:' + err)};
+var newAuthor = new authorModel();
+newAuthor.name = 'Wang';
+newAuthor.email = 'crazykevin777@gmail.com';
+newAuthor.save(adderr);
+}
 
 exports.getAll = function(req, res){
-
-console.log('before');
 var mongoose = require('mongoose');
 var conn = mongoose.createConnection('mongodb://localhost/Gallerydb');
-
-console.log('before schema');
-
-var albums = new mongoose.Schema({
-name: {type: String, requiered: true},
-author: { type: String, required: true, ref: 'author'},
-metadata: {
-	description: String,
-	date_added: { type: Date, default: Date.now }},
-comments: [{
-	date: { type: Date, default: Date.now },
-	text: String,
-	author: { type: String, required: true, ref: 'author' }
-}]
-});
-albums.index({name:1,author:1},{unique:true});
+var albums = createSchema();
 var albumModel = conn.model('album', albums);
-
-console.log('after model');
 
 var query = albumModel.find();
 
